11485761ee23f3cb3cf23b2d0f7d36dd880c4f80
hdshashank <hdshashankgowda@gmail.com>
2024-01-26 21:15:21 +0530

    Webpage

---
+++ script.js
@@ -0,0 +1,8 @@
+function showmenu(){
+    const menu=document.querySelector(".menu-links");
+    menu.style.display='flex';
+}
+function closemenu(){
+    const close=document.querySelector(".menu-links");
+    close.style.display='none';
+}
